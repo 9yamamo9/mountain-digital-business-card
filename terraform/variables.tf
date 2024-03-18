1f35c68eb85d08e8c81d8836320bcad1fd996e8d
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-18 20:48:31 +0900

    add: a infrastructure code for cloudlfare pages project by terraform.

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "cloudflare_api_token" {
+  description = "This value is for a cloudflare api token."
+  type = string
+}
+
+variable "cloudflare_account_id" {
+  description = "This value is for a cloudflare account id."
+  type = string
+}

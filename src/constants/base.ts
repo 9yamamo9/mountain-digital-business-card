adecbee67b5182c8788cf715681cf8f68ccef33f
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-20 13:49:02 +0900

    add: meta data for open graph protocol in head.

---
+++ src/constants/base.ts
@@ -0,0 +1,3 @@
+export const SITE_URL = 'https://mountain-digital-business-card.pages.dev'
+export const SITE_NAME = 'Mt. Three Trees Business Card'
+export const SITE_DESCRIPTION = 'This is a digital business card for me.'

134563c33a93aef5751e3721763da7f6b5117c4a
Yuta Yamamori <yamamori929yuta@gmail.com>
2024-03-20 23:41:35 +0900

    mod: a message in QR Code Page.

--- src/QrCode.tsx
+++ src/QrCode.tsx
@@ -9,7 +9,7 @@ const QrCode = () => {
 			<main className="max-w-[720px] max-md:max-w-full m-auto">
 				<div className="flex flex-col items-center m-4">
 					<img className="w-42 h-42" src={qrCode} />
-					<h1>For Access</h1>
+					<h1 className="text-gray-500">For Access my business card.</h1>
 				</div>
 			</main>
 			<Footer />
